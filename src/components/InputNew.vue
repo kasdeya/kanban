a3be53835056f2d1e1a1fc1c7886dc8b6ae4edc4
kasdeya <kas@nnnetwork.xyz>
2023-05-15 18:07:42 -0700

    First commit

---
+++ src/components/InputNew.vue
@@ -0,0 +1,29 @@
+<template>
+    <form action="" @submit.prevent="handleSubmit">
+    <input type="text" v-model="text">
+</form>
+</template>
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const text = ref('');
+const emits = defineEmits(['onNewItem'])
+
+function handleSubmit() {
+    if (text !== '') {
+        emits('onNewItem', text)
+        text.value = ""
+    }
+}
+</script>
+<style scoped>
+
+form {
+    margin-bottom: 10px;
+}
+
+input {
+    width: 100%;
+    box-sizing: border-box;
+}
+</style>
